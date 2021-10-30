8a7f08337ccb0680441855c95cf1a246bfbf59c2
stuartdambi@gmail.com <stuartdambi@gmail.com>
2021-10-30 13:47:57 +0300

    [add] wrap app with redux provider

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
 import RTLLayout from "layouts/RTL.js";
-import AuthProvider from "context/AuthContext";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import configureStore from "./store";
+
+const { persistor, store } = configureStore();
 
 ReactDOM.render(
-  <AuthProvider>
-    <HashRouter>
-      <Switch>
-        <Route path={`/auth`} component={AuthLayout} />
-        <Route path={`/admin`} component={AdminLayout} />
-        <Route path={`/rtl`} component={RTLLayout} />
-        <Redirect from={`/`} to="/auth" />
-      </Switch>
-    </HashRouter>
-  </AuthProvider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <HashRouter>
+          <Switch>
+            <Route path={`/auth`} component={AuthLayout} />
+            <Route path={`/admin`} component={AdminLayout} />
+            <Route path={`/rtl`} component={RTLLayout} />
+            <Redirect from={`/`} to="/auth" />
+          </Switch>
+        </HashRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
